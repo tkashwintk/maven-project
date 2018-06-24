4a3a98f2c09b0748c288fafdec68c59d51740f6c
AshwinTk <ashwintkkerala@gmail.com>
2018-06-24 16:16:18 +0530

    sample2

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,12 +11,13 @@ public class Greeter {
   public Greeter() {
 
   }
-/**
- * 
- * @param someone
- * @return
- */
-  //main method
+
+  
+  /**
+   * 
+   * @param someone
+   * @return
+   */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
